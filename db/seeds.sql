140f5df7399c2e7365befce4eb689373a89abdf9
Kevin Beeler <kbeeler07@gmail.com>
2018-05-04 23:54:06 -0400

    first Hw commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Classic', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western BBQ', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster', false);
